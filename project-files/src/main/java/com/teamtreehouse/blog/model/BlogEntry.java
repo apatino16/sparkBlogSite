f223c489dbb76caf4b5c99eb30a8edc4cecd4aac
Anny Patino <103137175+apatino16@users.noreply.github.com>
2024-12-02 13:29:04 -0800

    Create the Models
    Expanded the provided BlogEntry and Comment class with necessary attributes and methods.

--- project-files/src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
+++ project-files/src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -1,8 +1,56 @@
 package com.teamtreehouse.blog.model;
 
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class BlogEntry {
-    public boolean addComment(Comment comment) {
-        // Store these comments!
-        return false;
+
+    private String title;
+    private String content;
+    private LocalDateTime date;
+    List<Comment> comments;
+
+    // Constructor
+    public BlogEntry(String title, String content, LocalDateTime date) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.comments = new ArrayList<>();
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+
+    // Methods to manage comments
+    public void addComment(Comment comment) {
+       comments.add(comment);
+    }
+
+    public List<Comment> getComments(){
+        return comments;
     }
 }
--- project-files/src/main/java/com/teamtreehouse/blog/model/Comment.java
+++ project-files/src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -1,4 +1,41 @@
 package com.teamtreehouse.blog.model;
 
+import java.time.LocalDateTime;
+
 public class Comment {
+    private String author;
+    private String content;
+    private LocalDateTime date;
+
+    // Constructor
+    public Comment(String author, String content, LocalDateTime date) {
+        this.author = author;
+        this.content = content;
+        this.date = date;
+    }
+
+    // Getters and Setters
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 }
